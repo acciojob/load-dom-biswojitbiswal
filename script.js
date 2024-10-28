04dfa28e284ad0d21ba07a6ed73f500ddac36387
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-28 10:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded', () => {
     const textNode = document.createTextNode("DOM load success");
-    
+
     document.body.appendChild(textNode);
 });
